7b3dda227da12e8d5757c8f65994a2c0a79f730e
Michael Lutton <michael.lutton@gmail.com>
2013-07-25 02:10:14 -0400

    Added two cell integration tests

---
+++ CookieMonsterQuiz.Tests/Integration/TwoTileForestTests.cs
@@ -0,0 +1,42 @@
+﻿using System.Linq;
+using CookieMonsterQuiz.Tests.Utilities;
+using NUnit.Framework;
+
+namespace CookieMonsterQuiz.Tests.Integration
+{
+    [TestFixture]
+    class TwoTileForestTests
+    {
+        [Test]
+        public void TestThatTwoByOneTileForestCompletesSuccessfully()
+        {
+            var cookieMonsterParser = new CookieForestParser();
+            var cookieMonster = new CookieMonster(cookieMonsterParser);
+
+            var twoByOneForest = ForestBuilder.BuildForestOfSize(2, 1);
+
+            var result = cookieMonster.FindPathThroughForest(twoByOneForest);
+
+            Assert.That(result, Has.Count.EqualTo(2));
+            Assert.That(result.ElementAt(0).X, Is.EqualTo(1));
+            Assert.That(result.ElementAt(0).Y, Is.EqualTo(1));
+            Assert.That(result.ElementAt(1).X, Is.EqualTo(2));
+            Assert.That(result.ElementAt(1).Y, Is.EqualTo(1));
+        }
+
+        [Test]
+        public void TestThatOneByTwoTileForestCompletesSuccessfully()
+        {
+            var cookieMonsterParser = new CookieForestParser();
+            var cookieMonster = new CookieMonster(cookieMonsterParser);
+
+            var twoByOneForest = ForestBuilder.BuildForestOfSize(1, 2);
+
+            var result = cookieMonster.FindPathThroughForest(twoByOneForest);
+
+            Assert.That(result, Has.Count.EqualTo(1));
+            Assert.That(result.ElementAt(0).X, Is.EqualTo(1));
+            Assert.That(result.ElementAt(0).Y, Is.EqualTo(1));
+        }
+    }
+}
