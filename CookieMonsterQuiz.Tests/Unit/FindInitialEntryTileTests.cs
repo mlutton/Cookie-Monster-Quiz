140f32584ddd31a3c206593350cdbe88e7d9412f
Michael Lutton <michael.lutton@gmail.com>
2013-07-24 22:15:19 -0400

    Added additional test around checking that the initial tile list has a valid tile at coordinate 1,1

--- CookieMonsterQuiz.Tests/Unit/FindInitialEntryTileTests.cs
+++ CookieMonsterQuiz.Tests/Unit/FindInitialEntryTileTests.cs
@@ -24,5 +24,15 @@ namespace CookieMonsterQuiz.Tests.Unit
             Assert.Throws<ArgumentOutOfRangeException>(
                 () => cookieForestParser.FindInitialEntryTile(new List<CookieMonsterTile>()));
         }
+
+        [Test]
+        public void TestThatFindInitialEntryTileThrowsWhenPassingInInvalidTileList()
+        {
+            var cookieForestParser = new CookieForestParser();
+            var invalidOneByOneForest = new List<CookieMonsterTile> {new CookieMonsterTile() {X = 9999, Y = 9999}};
+
+            Assert.Throws<ArgumentOutOfRangeException>(
+                () => cookieForestParser.FindInitialEntryTile(invalidOneByOneForest));
+        }
     }
 }
