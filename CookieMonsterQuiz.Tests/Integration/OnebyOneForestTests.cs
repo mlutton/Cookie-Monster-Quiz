7699766b298273d38f42d49c002aacd8251e2889
Michael Lutton <michael.lutton@gmail.com>
2013-07-25 02:02:03 -0400

    Added integration test for 1x1 Forest

---
+++ CookieMonsterQuiz.Tests/Integration/OnebyOneForestTests.cs
@@ -0,0 +1,25 @@
+﻿using System.Linq;
+using CookieMonsterQuiz.Tests.Utilities;
+using NUnit.Framework;
+
+namespace CookieMonsterQuiz.Tests.Integration
+{
+    [TestFixture]
+    class OnebyOneForestTests
+    {
+        [Test]
+        public void TestThatOneByOneForestReturnsItself()
+        {
+            var cookieMonsterParser = new CookieForestParser();
+            var cookieMonster = new CookieMonster(cookieMonsterParser);
+
+            var oneByOneForest = ForestBuilder.BuildForestOfSize(1, 1);
+
+            var result = cookieMonster.FindPathThroughForest(oneByOneForest);
+
+            Assert.That(result.First.Value.X, Is.EqualTo(CookieForestParser.InitialXPosition));
+            Assert.That(result.First.Value.Y, Is.EqualTo(CookieForestParser.InitialYPosition));
+
+        }
+    }
+}
